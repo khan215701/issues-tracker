dc495391142bb936d5086e9c68fd4598ff4ead87
khan safiq <>
2023-12-09 12:19:46 +0530

    design naavbar with logo

---
+++ app/NavBar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { FaBug } from "react-icons/fa";
+const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b h-14 mb-5 px-5 items-center">
+      <Link href="/">
+        <FaBug />
+      </Link>
+      {links.map((link) => (
+        <Link
+          key={link.href}
+          href={link.href}
+          className="text-zinc-500 hover:text-white"
+        >
+          {link.label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default NavBar;
